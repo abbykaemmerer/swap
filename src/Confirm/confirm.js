98d67cf4eb61348ae88e05518187caee0c93473d
kaemmerer.abby@gmail.com <akaemmerer@vermeer.com>
2023-12-19 22:47:09 -0600

    12.19: set up

---
+++ src/Confirm/confirm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ConfirmScreen = ( props ) => {
+
+
+};
+
+export default ConfirmScreen;
---
+++ src/RSVP/rsvp.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class InviteForm extends React.Component {
+  
+  }
+
+export default InviteForm;
