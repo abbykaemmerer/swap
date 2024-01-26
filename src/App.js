fd72f6e76c54edb1c9986c4ff932fbde23bc6fe9
kaemmerer.abby@gmail.com <akaemmerer@vermeer.com>
2024-01-26 16:20:43 -0600

    1.26: clean up

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import InviteScreen from './Invite/invite';
 import GuestListScreen from './GuestList/guest-list';
-import RSVPFormScreen from './RSVPForm/rsvp';
 import FAQScreen from './FAQ/faq';
-import { nanoid } from "nanoid";
 
 function App() {
-const savedList = JSON.parse(localStorage.getItem('savedGuestList'));
-const initialState={
-  list:savedList ? savedList : []
-}
-const [name, setName] = useState();
-const [contribution, setContribution] = useState();
-
-const [list, setList] = useState(initialState.list);
-
-useEffect(()=>{
-  localStorage.setItem('savedGuestList', JSON.stringify(list))
-})
-// const handleNameChange = (e) => {
-//   setName(e.target.value);
-// };
-
-// const handleContributionChange = (e) => {
-//   setContribution(e.target.value);
-// };
-
-
-// const handleConfirm = () => {
-//   const newGuest = {
-//     name: name,
-//     contribution: contribution,
-//     id: nanoid(),
-//   };
-//   setName("");
-//   setContribution("");
-//   setList([...list, newGuest]);
-// }
-
-// const handleDelete = (guest) => {
-//   let newlist = list.map((guest) => ({ ...guest }));
-//   setList(newlist.filter((g) => g.id !== guest.id));
-// };
 
 
   return (
