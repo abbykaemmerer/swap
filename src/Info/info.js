09800f7733c3282cd828764588d1d0091bc236f2
kaemmerer.abby@gmail.com <akaemmerer@vermeer.com>
2024-01-28 17:30:36 -0600

    1.28: minor wording and css tweaks

--- src/Info/info.js
+++ src/Info/info.js
@@ -7,16 +7,14 @@ return (
             <h1>What is a clothing swap?</h1>
             <p>First and foremost, a partyyyyy!</p>
             <p>Clothing swaps are used to exchange clothes and items within your community.</p>
-            <p>Swaps are hugely impactful and a big part of promoting slow, circular fashion. </p>
             <p>Swapping clothes with your friends extends the lifespan of the item and is a 
             responsible way to rehome clothing. </p>
-      
-            <p>Unfortunately when we donate clothes, a very small portion actually gets put 
-            redistributed. The majority get shipped to other countries and winds up in their 
+            <p>Unfortunately when we donate clothes, a very small portion actually gets 
+            redistributed. The majority get shipped to other countries and wind up in their 
             landfills. Making our waste another countries' problem.</p> 
             <p><a href="https://www.consciouslifeandstyle.com/what-happens-to-donated-clothes/" target='_blank' rel="noreferrer">America.</a></p>
             <p>🙄</p>
-            <p>So thank YOU for taking the first step in slowing down your clothing consumption!</p>
+            <p>So thank YOU for taking a step in slowing down your clothing consumption!</p>
             <p>I hope you're ready to find some dope ass swap swaAag!</p>
             </div>
     </div>
